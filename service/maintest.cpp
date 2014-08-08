b4b5fdfa217ec562832300902da667c40f5fd28a
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-08-08 14:02:16 +0000

    BHttpServer: Start writing a simple test app

--- service/maintest.cpp
+++ service/maintest.cpp
@@ -1,12 +1,21 @@
 
+#include <NetEndpoint.h>
+
 #include <stdio.h>
+#include "StreamingRingBuffer.h"
+#include "HttpServer.h"
+#include "HttpHandler.h"
 
 
 int
 main()
 {
+	BNetEndpoint* receiveEndpoint = new(std::nothrow) BNetEndpoint();
 
-
+	// TODO: Replace StreamingRingBuffer with BNetBuffer or BDynamicBuffer
+	StreamingRingBuffer* sendBuffer = new(std::nothrow) StreamingRingBuffer(16 * 1024);
+	BHttpServer* webServer = new(std::nothrow) BHttpServer(receiveEndpoint);
+	HttpHandler* handler = new(std::nothrow) HttpHandler("output", sendBuffer);
 
 	return 0;
 }
